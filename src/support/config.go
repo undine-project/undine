083977e0216f3a9826d056351e65a75b97bf04d7
Pozitronik <antikillerxxl@gmail.com>
2024-10-31 11:50:19 +0400

    #4 #6 #7 Move config related code to the separate namespace (temporarily)

---
+++ src/support/config.go
@@ -0,0 +1,45 @@
+package support
+
+import (
+	"fmt"
+	"github.com/undine-project/undine/src/builder"
+	"gopkg.in/yaml.v3"
+	"log"
+	"os"
+)
+
+type Config struct {
+	TemplatePath string                   `yaml:"templatePath"`
+	Files        []builder.FileDefinition `yaml:"files"`
+}
+
+// LoadConfig Temporary, until #1 implemented
+func LoadConfig() Config {
+	configFile := "docs-config.yaml"
+
+	yamlFile, err := os.Open(configFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(yamlFile *os.File) {
+		_ = yamlFile.Close()
+	}(yamlFile)
+
+	c := Config{}
+	yamlDecoder := yaml.NewDecoder(yamlFile)
+	err = yamlDecoder.Decode(&c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if c.TemplatePath == "" {
+		panic("template path is empty")
+	}
+
+	_, err = os.Stat(c.TemplatePath)
+	if err != nil {
+		panic(fmt.Sprintf("template file %s doesn't exist", c.TemplatePath))
+	}
+
+	return c
+}
